2cd26e15e0dce46e7b577962f605de579d72aa15
John Dana <john@danasd.com>
2023-02-04 15:39:14 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`title = ${title}, color=${color}`)
+  }
+  return (
+    <form onSubmit={submit}>
+      <input type="text" 
+        value={title} 
+        onChange={event => setTitle(event.target.value)} 
+        placeholder='Input a color...' 
+      />
+      <input type="color" value={color} onChange={event => setColor(event.target.value)} />
+      <button>ADD</button>
+    </form>
+  );
+}
+
+export default App;
