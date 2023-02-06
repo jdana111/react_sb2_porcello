3ae8867262ce0a0cb0c38f07fdbbf07c993bc572
John Dana <john@danasd.com>
2023-02-06 16:50:24 -0700

    06_02 Rendering data from an API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
+
+function GitHubUser({ name, location, avatar_url }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar_url} height="150" />
+    </div>
+  );
+}
 
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch("https://api.github.com/users/jdana111")
       .then((response) => response.json())
-      .then(setData);
+      .then((data) => setData(data));
   }, []);
-  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>);
-  return <h1>Data</h1>;
+  if (data) return <GitHubUser name={data.name} location={data.location} avatar_url={data.avatar_url} />;
 }
 
 export default App;
