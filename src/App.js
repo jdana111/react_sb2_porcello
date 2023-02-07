7dec0151e32a7789e0ba15783d2b53dce8438b75
John Dana <john@danasd.com>
2023-02-07 16:55:07 -0700

    06 05 Component properties on roids

--- src/App.js
+++ src/App.js
@@ -1,59 +1,38 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name, 
-    elevationGain, 
-    status
+let cars = [
+  {
+    "color": "purple",
+    "type": "minivan",
+    "registration": new Date('2017-01-03'),
+    "capacity": 7
+  },
+  {
+    "color": "red",
+    "type": "station wagon",
+    "registration": new Date('2018-03-03'),
+    "capacity": 5
   }
-}`;
+]
 
-const ops = { 
-  method: "POST", 
-  headers: { "Content-Type": "application/json"}, 
-  body: JSON.stringify({query})
-};
-
-function SkiLift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
-  );
+function List({data, renderItem, renderEmpty}) {
+  return !data.length ? renderEmpty : 
+  <ul>{
+    data.map((e, i) => (<li key={i}>{renderItem(e)}</li>))
+  }</ul>
 }
 
-function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://snowtooth.moonhighway.com/`, ops)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error)}</pre>;
-  if (data)
-    return (
-      <div>
-        {data.data.allLifts.map((lift, i) => (
-          <SkiLift
-            key={i}
-            name={lift.name}
-            elevationGain={lift.elevationGain}
-            status={lift.status}
-          />
-        ))}
-      </div>
-    );
+function App() { return (
+  <List 
+    data={cars} 
+    renderEmpty={<p>This list is empty.</p>} 
+    renderItem={(e) => (
+      <>
+        {e.color} {e.type}, capacity: {e.capacity}
+      </>
+    )}
+  />
+)  
 }
 
 export default App;
