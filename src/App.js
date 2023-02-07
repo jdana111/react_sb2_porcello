af856e6ee2e04980b5298ad3cc93eb92f07745be
John Dana <john@danasd.com>
2023-02-06 17:58:15 -0700

    06 03 Handling loading states

--- src/App.js
+++ src/App.js
@@ -13,11 +13,18 @@ function GitHubUser({ name, location, avatar_url }) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
   useEffect(() => {
+    setLoading (true);
     fetch("https://api.github.com/users/jdana111")
       .then((response) => response.json())
-      .then((data) => setData(data));
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return (<pre>{JSON.stringify(error)}</pre>)
   if (data) return <GitHubUser name={data.name} location={data.location} avatar_url={data.avatar_url} />;
 }
 
