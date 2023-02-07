925c0feab9830ee53a5a6f3c0e29e4312b615473
John Dana <john@danasd.com>
2023-02-07 06:02:57 -0700

    06 04 Using the GraphQL simulated API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GitHubUser({ name, location, avatar_url }) {
+const query = `
+query {
+  allLifts {
+    name, 
+    elevationGain, 
+    status
+  }
+}`;
+
+const ops = { 
+  method: "POST", 
+  headers: { "Content-Type": "application/json"}, 
+  body: JSON.stringify({query})
+};
+
+function SkiLift({ name, elevationGain, status }) {
   return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar_url} height="150" />
+      <p>
+        {elevationGain} {status}
+      </p>
     </div>
   );
 }
@@ -16,16 +32,28 @@ function App() {
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
   useEffect(() => {
-    setLoading (true);
-    fetch("https://api.github.com/users/jdana111")
+    setLoading(true);
+    fetch(`https://snowtooth.moonhighway.com/`, ops)
       .then((response) => response.json())
       .then(setData)
       .then(() => setLoading(false))
       .catch(setError);
   }, []);
   if (loading) return <h1>Loading...</h1>;
-  if (error) return (<pre>{JSON.stringify(error)}</pre>)
-  if (data) return <GitHubUser name={data.name} location={data.location} avatar_url={data.avatar_url} />;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (data)
+    return (
+      <div>
+        {data.data.allLifts.map((lift, i) => (
+          <SkiLift
+            key={i}
+            name={lift.name}
+            elevationGain={lift.elevationGain}
+            status={lift.status}
+          />
+        ))}
+      </div>
+    );
 }
 
 export default App;
