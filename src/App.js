8ccc7a3f169514f5d6b88d4d370086629e1680fd
John Dana <john@danasd.com>
2023-02-07 18:16:27 -0700

    07 01 Part a, install react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import "./App.css";
 
-let cars = [
-  {
-    "color": "purple",
-    "type": "minivan",
-    "registration": new Date('2017-01-03'),
-    "capacity": 7
-  },
-  {
-    "color": "red",
-    "type": "station wagon",
-    "registration": new Date('2018-03-03'),
-    "capacity": 5
-  }
-]
-
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ? renderEmpty : 
-  <ul>{
-    data.map((e, i) => (<li key={i}>{renderItem(e)}</li>))
-  }</ul>
-}
-
-function App() { return (
-  <List 
-    data={cars} 
-    renderEmpty={<p>This list is empty.</p>} 
-    renderItem={(e) => (
-      <>
-        {e.color} {e.type}, capacity: {e.capacity}
-      </>
-    )}
-  />
-)  
+function App() { return <h1>App</h1>
 }
 
 export default App;
